89a4dff8c5cb83da44bea1615155452bdd67c430
Guilder W. Milliner <guilderw@gmail.com>
2024-08-30 00:28:22 -0600

    chapter 2 complete

--- TDD-ES6/src/letter-count.test.js
+++ TDD-ES6/src/letter-count.test.js
@@ -1,9 +1,20 @@
-import { expect } from 'chai';import { getLetterCount } from "./letter-count.js";
+import { expect } from "chai";
+import { getLetterCount } from "./letter-count.js";
 
-describe('getLetterCount - basic functionality', () => {
-    it('returns an empty object when passed an empty string', () => {
-        const expected = {};
-        const actual = getLetterCount('');
+describe("getLetterCount - basic functionality", () => {
+  it("returns an empty object when passed an empty string", () => {
+    const expected = {};
+    const actual = getLetterCount("");
+    expect(actual).to.deep.equal(expected);
+  });
+    it("return the correct letter count for a word with only one of each letter", () => {
+        const expected = { c: 1, a: 1, t: 1 };
+        const actual = getLetterCount("cat");
         expect(actual).to.deep.equal(expected);
-    })
-})
+    });
+    it("return the correct letter count for words with more than one of a letter", () => {
+        const expected = { b: 1, e: 2, t: 2, r: 1 };
+        const actual = getLetterCount("better");
+        expect(actual).to.deep.equal(expected);
+    });
+});
