ec2c25bd48e9434a710b159bddb2c131fbd214ed
Guilder W. Milliner <guilderw@gmail.com>
2024-10-10 11:52:18 -0600

    Setting up Frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.vue', 'src/**/*.ts'], // Lint .vue and .ts files
+    }),
+  ],
+  resolve: {
+    alias: {
+      // Simplifies imports by allowing '@' to map to the /src folder
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  build: {
+    // Example of customizing the build
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'pinia', 'vue-router'],
+        },
+      },
+    },
+  },
 });
