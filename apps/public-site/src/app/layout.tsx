5ca6ac4555b824c18c2faf9edfa0bee505c32ae5
Guilder W. Milliner <guilderw@gmail.com>
2024-10-15 16:16:36 -0600

    Setup metadata

--- apps/public-site/src/app/layout.tsx
+++ apps/public-site/src/app/layout.tsx
@@ -1,4 +1,5 @@
 // apps/public-site/src/app/layout.tsx
+import React from "react";
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
 
@@ -8,8 +9,41 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "My App",
-  description: "My Next.js application",
+  title: "DebtFreePlanner",
+  description: "Strategically plan your journey to a Debt-Free Living.",
+  author: "GUilder W. Milliner",
+  keywords: [
+    "Debt Management",
+    "Financial Planning",
+    "Debt-Free",
+    "Debt Payoff",
+    "Budgeting",
+    "Finance",
+  ],
+  openGraph: {
+    title: "DebtFreePlanner - Plan your journey to a Debt-Free Living",
+    description:
+      "Take control of your finances and manage your debt effectively with DebtFreePlanner. Achieve financial freedom by strategically planning your debt payoff.",
+    url: "https://debtfreeplanner.app",
+    siteName: "DebtFreePlanner",
+    images: [
+      {
+        url: "https://debtfreeplanner.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "DebtFreePlanner - Manage Your Debt Effectively",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "DebtFreePlanner",
+    description: "Strategically plan your journey to a Debt-Free Living.",
+    creator: "@DebtFreePlanner",
+    image: "https://debtfreeplanner.app/twitter-card.jpg",
+  },
 };
 
 // Define a read-only props interface
