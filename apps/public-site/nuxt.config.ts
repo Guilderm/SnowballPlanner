0d507c210118951fa3f4e15fb515a91f6af5da4b
Guilder W. Milliner <guilderw@gmail.com>
2024-10-22 09:45:40 -0600

    Refactor nuxt.config.ts to remove unnecessary console.log statement

--- apps/public-site/nuxt.config.ts
+++ apps/public-site/nuxt.config.ts
@@ -84,7 +84,6 @@ export default defineNuxtConfig({
       console.log('AUTH0_DOMAIN:', process.env.AUTH0_DOMAIN)
       console.log('AUTH0_CLIENT_ID:', process.env.AUTH0_CLIENT_ID)
       console.log('GOOGLE_GTAG_ID:', process.env.GOOGLE_GTAG_ID)
-      console.log('NODE_ENV:', process.env.NODE_ENV)
     },
   },
 })
