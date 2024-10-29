91ba4f5b7b36d43d7d0b591b3a6f5bcbceabd7cd
Guilder W. Milliner <guilderw@gmail.com>
2024-10-29 08:21:12 -0600

    Refactor package.json scripts to use npm run commands with correct prefixes

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 // prettier.config.js
 
-/** @type {import("prettier").Config} */
 export default {
   semi: false,
   singleQuote: true,
   trailingComma: 'es5',
   printWidth: 80,
   tabWidth: 2,
+  plugins: [import('prettier-plugin-tailwindcss')]
 };
