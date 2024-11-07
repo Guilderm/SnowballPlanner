6beeb9389c1b8c480d2ea8b92639a43d984fe9f6
Guilder W. Milliner <guilderw@gmail.com>
2024-11-07 09:32:15 -0600

    Refactor code formatting and update package.json description: improve consistency in cloudbuild.yaml, enhance readability in main.ts, and modify project description in package.json

--- apps/pwa-server/src/main.ts
+++ apps/pwa-server/src/main.ts
@@ -1,39 +1,39 @@
 // apps\pwa-server\src\main.ts
 
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.js';
-import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module.js'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
 
   // Retrieve allowed origins from environment variable and split into an array
   const allowedOrigins =
     configService
       .get<string>('ALLOWED_ORIGINS')
       ?.split(',')
-      .map((origin) => origin.trim()) ?? [];
+      .map((origin) => origin.trim()) ?? []
 
   // Log the allowed origins for debugging
-  console.log(`CORS allowed origins: ${allowedOrigins.join(', ')}`);
+  console.log(`CORS allowed origins: ${allowedOrigins.join(', ')}`)
 
   // Enable CORS using NestJS's built-in CORS support
   app.enableCors({
     origin: (origin, callback) => {
       // Allow requests with no origin (like server-to-server or Postman requests)
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
+        callback(null, true)
       } else {
-        console.error(`Blocked by CORS policy for origin: ${origin}`);
-        callback(new Error(`Not allowed by CORS for origin: ${origin}`));
+        console.error(`Blocked by CORS policy for origin: ${origin}`)
+        callback(new Error(`Not allowed by CORS for origin: ${origin}`))
       }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allows cookies to be sent cross-origin if necessary
-  });
+  })
 
   // Enable global validation pipes
   app.useGlobalPipes(
@@ -42,12 +42,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       transform: true,
     })
-  );
+  )
 
   // Define the port from environment variable with a default fallback
-  const PORT = process.env.PORT ?? configService.get<number>('PWA_SERVER_PORT') ?? 3300;
-  await app.listen(PORT);
-  console.log(`pwa-server is running on port ${PORT}`);
+  const PORT =
+    process.env.PORT ?? configService.get<number>('PWA_SERVER_PORT') ?? 3300
+  await app.listen(PORT)
+  console.log(`pwa-server is running on port ${PORT}`)
 }
 
-bootstrap();
+bootstrap()
