99988781efad3a8df64a8356442584d47473301c
Guilder W. Milliner <guilderw@gmail.com>
2024-11-13 14:53:44 -0600

    Update import statements in pwa-server: add .js extensions for module imports in newsletter.controller and app.e2e-spec files

--- apps/pwa-server/src/newsletter/newsletter.controller.ts
+++ apps/pwa-server/src/newsletter/newsletter.controller.ts
@@ -1,13 +1,15 @@
+// apps/pwa-server/src/newsletter/newsletter.controller.ts
+
 import { Controller, Post, HttpCode, HttpStatus, Body } from "@nestjs/common";
-import { NewsletterService } from "./newsletter.service";
+import { NewsletterService } from "./newsletter.service.js";
 import { ConfigService } from "@nestjs/config";
-import { SubscribeDto } from "./dto/subscribe.dto";
+import { SubscribeDto } from "./dto/subscribe.dto.js";
 
 @Controller("api/newsletter_subscription")
 export class NewsletterController {
   constructor(
     private readonly newsletterService: NewsletterService,
-    private readonly configService: ConfigService, // Inject ConfigService
+    private readonly configService: ConfigService,
   ) {}
 
   @Post()
@@ -15,7 +17,6 @@ export class NewsletterController {
   async subscribe(
     @Body() subscribeDto: SubscribeDto,
   ): Promise<{ message: string }> {
-    // Example of getting a configuration value
     const appEnv = this.configService.get<string>("NODE_ENV");
     console.log(`App environment: ${appEnv}`);
 
